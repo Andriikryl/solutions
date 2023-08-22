692a069d4c6aaaee26c1f41f843d469825c6ac24
Andriikryl <andreyk557@gmail.com>
2023-08-22 18:05:02 +0300

    add loader fix

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,20 +3,22 @@ import style from "./loading.module.css";
 
 export default function Loading() {
   return (
-    <div className={style.body}>
-      <div className={style.scene}>
-        <div className={style.shadow} />
-        <div className={style.jumper}>
-          <div className={style.spinner}>
-            <div className={style.scaler}>
-              <div className={style.loader}>
-                <div className={style.cuboid}>
-                  <div className={style.cuboid__side} />
-                  <div className={style.cuboid__side} />
-                  <div className={style.cuboid__side} />
-                  <div className={style.cuboid__side} />
-                  <div className={style.cuboid__side} />
-                  <div className={style.cuboid__side} />
+    <div className="container">
+      <div className={style.body}>
+        <div className={style.scene}>
+          <div className={style.shadow} />
+          <div className={style.jumper}>
+            <div className={style.spinner}>
+              <div className={style.scaler}>
+                <div className={style.loader}>
+                  <div className={style.cuboid}>
+                    <div className={style.cuboid__side} />
+                    <div className={style.cuboid__side} />
+                    <div className={style.cuboid__side} />
+                    <div className={style.cuboid__side} />
+                    <div className={style.cuboid__side} />
+                    <div className={style.cuboid__side} />
+                  </div>
                 </div>
               </div>
             </div>
